df152c08aaae4fbdb2b22a728cd51fe618922d73
Enrique Rosales <erosales@domingoalonsogroup.com>
2023-03-22 17:53:26 +0000

    refactor: Hole 6. +3 points

--- csharp/Hole6/TakeHomeCalculator.cs
+++ csharp/Hole6/TakeHomeCalculator.cs
@@ -14,15 +14,7 @@ namespace Hole6
 
         public Money NetAmount(Money first, params Money[] rest)
         {
-            List<Money> monies = rest.ToList();
-
-            Money total = first;
-
-            foreach (Money next in monies)
-            {
-                total = total.Plus(next);
-            }
-
+            Money total = rest.Aggregate(first, (current, next) => current.Plus(next));
             Money tax = taxRate.Apply(total);
             return total.Minus(tax);
         }
